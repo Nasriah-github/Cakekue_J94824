7563368e1c80da73cc3ef3efe1365f36cc698e65
Nasriah-github <nurnasriah.saifullizan@gmail.com>
2023-04-04 22:54:25 +0800

    Simple Homepage updated.

---
+++ Pages/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Cakekue_J94824.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
