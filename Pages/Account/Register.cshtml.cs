ad16418ebae0ba24a619305832bd23b619605cbe
Nasriah-github <nurnasriah.saifullizan@gmail.com>
2023-06-22 02:18:35 +0800

    Updated code.

--- Pages/Account/Register.cshtml.cs
+++ Pages/Account/Register.cshtml.cs
@@ -7,31 +7,46 @@ namespace Cakekue_J94824.Pages.Account
 {
     public class RegisterModel : PageModel
     {
-
         [BindProperty]
         public RegistrationModel Input { get; set; }
 
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ILogger<RegisterModel> _logger;
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly UserManager<ApplicationUser> _userManager;
 
-
         public RegisterModel(
             UserManager<ApplicationUser> userManager,
-            SignInManager<ApplicationUser> signInManager)
+            SignInManager<ApplicationUser> signInManager,
+            RoleManager<IdentityRole> roleManager,
+            ILogger<RegisterModel> logger)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _roleManager = roleManager;
+            _logger = logger;
         }
 
         public async Task<IActionResult> OnPostAsync()
         {
             if (ModelState.IsValid)
             {
+                Task<bool> hasRegUserRole = _roleManager.RoleExistsAsync("RegisteredUser");
+                hasRegUserRole.Wait();
+
+                if (!hasRegUserRole.Result)
+                {
+                    var roleResult = _roleManager.CreateAsync(new IdentityRole("RegisteredUser"));
+                    roleResult.Wait();
+                }
+
                 var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
                     await _signInManager.SignInAsync(user, isPersistent: false);
+                    Task<IdentityResult> newUserRole = _userManager.AddToRoleAsync(user, "RegisteredUser");
+                    newUserRole.Wait();
                     return RedirectToPage("/Index");
                 }
                 foreach (var error in result.Errors)
@@ -39,20 +54,7 @@ namespace Cakekue_J94824.Pages.Account
                     ModelState.AddModelError(string.Empty, error.Description);
                 }
             }
-
-            // If we got this far, something failed, redisplay form
             return Page();
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
